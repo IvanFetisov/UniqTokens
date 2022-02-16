f6d9f7ecb605780662cd9fb19b0fdf97ebb67093
IvanFetisov <93977182+IvanFetisov@users.noreply.github.com>
2022-02-16 16:52:52 +0300

    Add files via upload
    This is first commit to my GitHub

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    Cipher cipher = new Cipher();
+    cipher.tokenSelection("A6u-IiN-Juh-Qb1");
+
+
+    }
+}
